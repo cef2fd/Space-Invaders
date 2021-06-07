c5da153e8c77b076e8dd5b033125b005fd0e1bf0
Charis Fortunato <83101528+cef2fd@users.noreply.github.com>
2021-06-06 23:13:22 -0400

    game complete with ship count

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,6 +80,8 @@ class AlienInvasion:
 			self.stats.reset_stats()
 			self.stats.game_active = True
 			self.sb.prep_score()
+			self.sb.prep_level()
+			self.sb.prep_ships()
 
 			# Get rid of any remaining aliens and bullets
 			self.aliens.empty()
@@ -141,11 +143,16 @@ class AlienInvasion:
 			self._create_fleet()
 			self.settings.increase_speed()
 
+			# Increase level
+			self.stats.level += 1
+			self.sb.prep_level()
+
 	def _ship_hit(self):
 		"""Respond to the ship being hit by an alien"""
 		if self.stats.ships_left > 0:
-			#Decrement ships_left
+			#Decrement ships_left and update scoreboard
 			self.stats.ships_left -= 1
+			self.sb.prep_ships()
 
 			# Get rid of any remaining aliens and bullets
 			self.aliens.empty()
